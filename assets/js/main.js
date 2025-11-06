461846cb9fa44199b35d92a1fa884e1881790823
azure-flow <bz3323766@gmail.com>
2025-11-06 10:39:24 +0900

    add: top-animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,6 +29,73 @@ document.addEventListener('DOMContentLoaded', function () {
             closeModal();
         }
     });
+
+    var swiper = new Swiper(".exp_swiper", {
+        slidesPerView: 3,
+        spaceBetween: 25,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false
+        },
+        speed: 2000, // Slow transition between slides to 2 seconds
+        breakpoints: {
+            // when window width is >= 0px (smartphone)
+            0: {
+                slidesPerView: 1,
+                slidesPerGroup: 1
+            },
+            // when window width is >= 768px (md size, and up)
+            768: {
+                slidesPerView: 3,
+                slidesPerGroup: 3
+            }
+        }
+    });
+
+    // featured Slider 
+    var featuredThumbsSwiper = new Swiper(".featured-thumbs-swiper", {
+        direction: 'vertical',
+        spaceBetween: 10,
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        loop: true,
+        // loopFillGroupWithBlank: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+    });
+    var featuredSwiper = new Swiper(".featured-swiper", {
+        spaceBetween: 10,
+        // slidesPerView: 1,
+        // watchSlidesVisibility: true,
+        // watchSlidesProgress: true,
+        // loop: true,
+        // autoplay: {
+        //     delay: 4000,
+        //     disableOnInteraction: false
+        // },
+        // speed: 2000,
+        pagination: {
+            el: '.featured-swiper .swiper-pagination',
+            clickable: true,
+            type: 'bullets',
+        },
+        thumbs: {
+            swiper: featuredThumbsSwiper,
+        },
+    });
+
+    document.querySelectorAll('.featured-thumbs-swiper .swiper-slide').forEach(function (slide) {
+        slide.addEventListener('mouseover', function (item) {
+            console.log('bot');
+            this.click();
+        });
+    });
 });
 
 
@@ -77,18 +144,10 @@ window.addEventListener('load', function () {
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    var swiper = new Swiper(".exp_swiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false
-        },
-        speed: 2000, // Slow transition between slides to 2 seconds
-    });
+
+
+    // Add click event to the .featured-thumbs-swiper .swiper-slide when hovered
+    // It will click the slide to trigger the Swiper navigation
 
     const hamburgerBtn = document.getElementById('hamburgerBtn');
     const hamburgerBtn_sp = document.getElementById('hamburgerBtn_sp');
